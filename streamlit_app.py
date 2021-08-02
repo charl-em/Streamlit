a9304396d5611637368689dbc26ace64f70ec56b
Kouadio charles emmanuel <dcharlesmanou@gmail.com>
2021-08-02 09:31:25 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ warnings.filterwarnings("ignore")
 
 st.title("Prédiction sur le statut du client de la banque")
 
-def file_selector(folder_path='C:/Users/Charly/Documents/Tp/'):
+def file_selector(folder_path='C:/Users/Charly/Documents/Tp/Streamlit/'):
 	    filenames = os.listdir(folder_path)
 	    selected_filename = st.selectbox('Select a file', filenames)
 	    return os.path.join(folder_path, selected_filename)
